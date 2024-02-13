48994d5a053cc6e911e49eca11d80d9fc664853d
shweeta jadhwani <108540615+ShweetaJadhwani@users.noreply.github.com>
2024-02-13 22:52:24 +0500

    Add files via upload

---
+++ Week-4/LabPractice/task.js
@@ -0,0 +1,32 @@
+console.log ("heyyy");
+console.error ("heyy");
+console.warn ("hyyy");
+
+
+//Variable Assign
+var a = 5;
+console.log(a);
+
+//function
+function greet(name) {
+    console.log("Hello, " + name + "!");
+    }
+    greet("John");
+
+    //try catch
+    try {
+       
+        var x = y / 0; 
+        } catch (error) {
+        
+        console.error("An error occurred:", error.message);
+        }
+
+    // function
+   
+
+
+function showAlert() {
+alert("Hello, world!");
+}
+        
