4b3bf301b481f665a14b26a0f118ebe8a48f010b
shweeta jadhwani <108540615+ShweetaJadhwani@users.noreply.github.com>
2024-05-31 17:08:56 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,12 @@
+function hello(){
+    //alert("Say heyyy");
+   const ball= document.createElement("div");
+   ball.className="ball";
+   const Container= document.getElementById("Container");
+   Container.appendChild(ball)
+   
+
+}
+// hello();
+button.onclick = hello;
+
